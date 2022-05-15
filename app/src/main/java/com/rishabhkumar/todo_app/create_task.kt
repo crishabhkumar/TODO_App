0b83331faca8502131517f0bc57a95622268783e
Rishabh Kumar <crishabhkumar@gmail.com>
2022-05-15 18:11:51 +0530

    UI Done.

---
+++ app/src/main/java/com/rishabhkumar/todo_app/create_task.kt
@@ -0,0 +1,11 @@
+package com.rishabhkumar.todo_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class create_task : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_task)
+    }
+}
