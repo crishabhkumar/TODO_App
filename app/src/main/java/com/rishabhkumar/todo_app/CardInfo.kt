1334c9d04465426e75e1b98e0f0f0a917c924b3a
Rishabh Kumar <crishabhkumar@gmail.com>
2022-05-15 18:48:08 +0530

    Adpater code setup is done.

---
+++ app/src/main/java/com/rishabhkumar/todo_app/CardInfo.kt
@@ -0,0 +1,6 @@
+package com.rishabhkumar.todo_app
+
+data class CardInfo(
+    var title: String,
+    var priority: String
+)
--- app/src/main/java/com/rishabhkumar/todo_app/CreateTask.kt
+++ app/src/main/java/com/rishabhkumar/todo_app/CreateTask.kt
@@ -3,9 +3,9 @@ package com.rishabhkumar.todo_app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class view : AppCompatActivity() {
+class CreateTask : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_view)
+        setContentView(R.layout.activity_create_task)
     }
 }
